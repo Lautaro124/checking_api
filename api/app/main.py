a03747d21a4b544503c93bbb79b1a0a0c1c2ab98
lautaro.gonzalez <lautaro.gonzalez@ctl.com.ar>
2023-06-11 23:43:53 -0300

    Authentication in client and cors

--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.router import router
 
 app = FastAPI()
 app.include_router(router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
--- api/app/routes/auth/auth.py
+++ api/app/routes/auth/auth.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+
+from app.config.header_auth import HTTPAuthentication
 from ...utils.auth import authenticate_user, verify_token
 router = APIRouter()
 
@@ -9,10 +11,13 @@ async def login(email: str):
     return HTTPException(status_code=403, detail="User does not exist")
   return { 'userId': user_id }
 
-@router.post("/authToken")
+@router.post("/sociaLogin", dependencies=[Depends(HTTPAuthentication())])
 def auth_token(token: str):
   token_verificed = verify_token(token)
-  print(token_verificed)
   if not token_verificed:
     return HTTPException(status_code=403, detail="Token does not exist")
   return { 'verified': True }
+
+@router.post("/logout", dependencies=[Depends(HTTPAuthentication())])
+def logout():
+  return { 'logout': True }
