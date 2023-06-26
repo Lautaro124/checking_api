f6fd18bd265b5d52694e2ff68a4993d8e5643867
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-06-26 19:35:57 -0300

    some schemas

--- api/app/core/schemas/companys.py
+++ api/app/core/schemas/companys.py
@@ -3,11 +3,14 @@ from ..db.database import Base
 from sqlalchemy.orm import relationship
 
 class Company(Base):
-  __tablename__ = 'companys'
+    __tablename__ = 'companys'
 
-  id = Column(Integer, primary_key=True, index=True)
-  name = Column(String, unique=True)
-  description = Column(String)
-  storage_id = Column(Integer, ForeignKey("storage.id", ondelete="SET NULL"), nullable=True)
-  users = relationship("User", back_populates="company")
-  storage = relationship("Storage", back_populates="company")
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    description = Column(String)
+    storage_id = Column(Integer, ForeignKey("storages.id", ondelete="SET NULL"), nullable=True)
+    product_id = Column(Integer, ForeignKey("products.id", ondelete="SET NULL"), nullable=True)
+    users = relationship("User", back_populates="companys")
+    storage = relationship("Storages", back_populates="companys")
+    product = relationship("Product",back_populates="companys")
+""
