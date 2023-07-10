f491a45fa221f4b7057e21f620826c4413b01121
lautaro.gonzalez <59624470+Lautaro124@users.noreply.github.com>
2023-07-10 16:09:05 -0300

    Fix HTML

--- client/src/app/(modules)/administration/layout.tsx
+++ client/src/app/(modules)/administration/layout.tsx
@@ -4,26 +4,19 @@ interface AdministrationLayoutProps {
   children: React.ReactNode
 }
 
-export const metadata = {
-  title: 'Administration',
-  description: 'Administration module for the application'
-}
-
 const AdministrationLayout = ({ children }: AdministrationLayoutProps) => {
   return (
-    <html>
-      <body className="w-screen h-screen">
-        <Header title="Admin">
-          <nav className="flex flex-row">
-            <a href="/">Home</a>
-            <a href="/">Users</a>
-          </nav>
-        </Header>
-        <main className="p-2">
-          {children}
-        </main>
-      </body>
-    </html>
+    <body className="w-screen h-screen">
+      <Header title="Admin">
+        <nav className="flex flex-row">
+          <a href="/">Home</a>
+          <a href="/">Users</a>
+        </nav>
+      </Header>
+      <main className="p-2">
+        {children}
+      </main>
+    </body>
   )
 }
 
