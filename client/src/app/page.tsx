09fb1aceb0f53b558dff99be51dec825c60ee97b
Lautaro Gonzalez <lautarogonzalez@Lautaros-MBP.fibertel.com.ar>
2023-07-12 00:02:27 -0300

    Out firebase

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,43 +1,10 @@
 'use client'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import InpitWithLabel from '~/components/molecules/inputWithLabel'
-import { auth } from '~/config/firebase'
+import Title from '~/components/atoms/title'
 
 export default function Home() {
-  const provider = new GoogleAuthProvider()
-
-  const loginWihtGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const credential = GoogleAuthProvider.credentialFromResult(result)
-        const token = credential?.idToken
-        console.log(token)
-        console.log(credential)
-        console.log(result)
-
-        void fetch(`http://localhost/auth/sociaLogin?token=${result.user.uid ?? ''}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${result.user.uid ?? ''}}`
-          }
-        })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Login</h1>
-      <InpitWithLabel label='Email' id='Email' type='email' />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={loginWihtGoogle}
-      >
-        Login with Google
-      </button>
+      <Title text='Welcome to my app' />
     </main>
   )
 }
